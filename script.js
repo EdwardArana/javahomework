df718af0f3617ceaacc246d4ffa439e59b79a430
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-13 15:15:01 -0800

    else if section

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = "0123456789".split("");
+var lcLetter = "abcdefghijklmnopqrstuvwxyz".split("");
+var ucLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var spLetter = "!@#$%^&*()_-+=`~[]{}".split("");
+var passwordLength = prompt("Specify a password length betweeen 8 and 128 ");
+var wantNumber = confirm("Would you like numbers?");
+var wantLc = confirm("Would you like lower case letters?");
+var wantUc = confirm("Would you like upper case letters?");
+var wantSp = confirm("Would you like special characters?");
+var ansArray = []
+var finArray =[]
+
+if (wantNumber) {
+  for(var i = 0; i < numbers.length; i++){
+
+   ansArray.push(wantNumber[i])
+  }
+} else  if (wantLc) {
+  for(var i = 0; i < lcLetter.length; i++){
+
+   ansArray.push(wantLc[i])
+  }
+}else if (wantUc) {
+  for(var i = 0; i < ucLetter.length; i++){
+
+    ansArray.push(wantUc[i])
+  }
+}else (wantSp) {
+  for(var i = 0; i < spLetter.length; i++){
+
+    ansArray.push(wantSp[i])
+  }
+};
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +46,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
